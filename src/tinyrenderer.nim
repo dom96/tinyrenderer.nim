479cd801aba0b75d76203b9d0244841500c8f7ee
Dominik Picheta <dominikpicheta@gmail.com>
2017-09-26 18:19:30 +0100

    Implement triangle fill.
    Equivalent to https://github.com/ssloy/tinyrenderer/tree/024ad4619b824f9179c86dc144145e2b8b155f52.

--- src/tinyrenderer.nim
+++ src/tinyrenderer.nim
@@ -1,6 +1,12 @@
-import math
+import math, basic2d
 
 import tgaimage, model
+
+
+let
+  white = constructTGAColor(255, 255, 255, 255)
+  red = constructTGAColor(255, 0, 0, 255)
+  green = constructTGAColor(0, 255, 0, 255)
 
 proc line(image: var TGAImage, x0, y0, x1, y1: int, color: TGAColor) =
   var (x0, y0, x1, y1) = (x0, y0, x1, y1)
@@ -23,28 +29,59 @@ proc line(image: var TGAImage, x0, y0, x1, y1: int, color: TGAColor) =
     else:
       discard image.set(x.cint, y.cint, color)
 
-let
-  white = constructTGAColor(255, 255, 255, 255)
-  red = constructTGAColor(255, 0, 0, 255)
+proc line(image: var TGAImage, a, b: Vector2d, color: TGAColor) =
+  image.line(a.x.int, a.y.int, b.x.int, b.y.int, color)
+
+proc triangle(image: var TGAImage, t0, t1, t2: Vector2d, color: TGAColor) =
+  var (t0, t1, t2) = (t0, t1, t2)
+  # Sort the vertices from lower-to-upper y.
+  if t0.y > t1.y: swap(t0, t1)
+  if t0.y > t2.y: swap(t0, t2)
+  if t1.y > t2.y: swap(t1, t2)
+
+  let totalHeight = t2.y - t0.y
+  template fillSegment(a, b) =
+    for y in countup(a.y, b.y, 1.0):
+      let segmentHeight = b.y - a.y
+      let alpha = (y.float - t0.y) / totalHeight
+      let beta = (y.float - a.y) / segmentHeight
+      var A = t0 + (t2-t0)*alpha
+      var B = a + (b-a)*beta
+
+      if A.x > B.x: swap(A, B)
+      for x in countup(A.x, B.x):
+        discard image.set(x.cint, y.cint, color)
+
+  fillSegment(t0, t1)
+  fillSegment(t1, t2)
+  image.line(t0, t1, color)
+  image.line(t1, t2, color)
+  image.line(t2, t0, color)
 
 
 const
   width = 800
   height = 800
 var image = constructTGAImage(width, height, RGB.cint)
-var head = newModel("african_head.obj")
 
-for i in 0 .. <head.nfaces():
-  let face = head.face(i)
-  for j in 0 .. <3:
-    let v0 = head.vert(face[j])
-    let v1 = head.vert(face[(j+1) mod 3])
+image.triangle(vector2d(10, 70), vector2d(50, 160), vector2d(70, 80), red)
+image.triangle(vector2d(180, 50), vector2d(150, 1), vector2d(70, 180), white)
+image.triangle(vector2d(180, 150), vector2d(120, 160), vector2d(130, 180),
+               green)
 
-    let x0 = (v0.x+1.0) * width/2
-    let y0 = (v0.y+1.0) * height/2
-    let x1 = (v1.x+1.0) * width/2
-    let y1 = (v1.y+1.0) * height/2
-    image.line(x0.cint, y0.cint, x1.cint, y1.cint, white)
+when false:
+  var head = newModel("african_head.obj")
+  for i in 0 .. <head.nfaces():
+    let face = head.face(i)
+    for j in 0 .. <3:
+      let v0 = head.vert(face[j])
+      let v1 = head.vert(face[(j+1) mod 3])
+
+      let x0 = (v0.x+1.0) * width/2
+      let y0 = (v0.y+1.0) * height/2
+      let x1 = (v1.x+1.0) * width/2
+      let y1 = (v1.y+1.0) * height/2
+      image.line(x0.cint, y0.cint, x1.cint, y1.cint, white)
 
 doAssert image.flipVertically()
 doAssert image.writeTgaFile("output.tga")
