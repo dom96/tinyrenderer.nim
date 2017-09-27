4d0fc36c8ae55117b21aaf438f6b414876a21a64
Dominik Picheta <dominikpicheta@gmail.com>
2017-09-27 16:23:57 +0100

    Implement zbuffer.
    This is on par (slightly ahead?) of https://github.com/ssloy/tinyrenderer/tree/68a5ae382135d679891423fb5285fdd582ca389d.

--- src/tinyrenderer.nim
+++ src/tinyrenderer.nim
@@ -2,6 +2,15 @@ import math, basic2d, random, basic3d
 
 import tgaimage, model
 
+type
+  ZBuffer = seq[seq[float]]
+
+proc newZBuffer(width, height: int): ZBuffer =
+  result = newSeq[seq[float]](width)
+  for x in 0 .. <width:
+    result[x] = newSeq[float](height)
+    for y in 0 .. <height:
+      result[x][y] = -INF
 
 let
   white = constructTGAColor(255, 255, 255, 255)
@@ -32,7 +41,7 @@ proc line(image: var TGAImage, x0, y0, x1, y1: int, color: TGAColor) =
 proc line(image: var TGAImage, a, b: Vector2d, color: TGAColor) =
   image.line(a.x.int, a.y.int, b.x.int, b.y.int, color)
 
-proc barycentric(t0, t1, t2: Vector2d, P: Vector2d): Vector3d =
+proc barycentric(t0, t1, t2: Vector3d, P: Vector2d): Vector3d =
   let u = cross(vector3d(t2.x-t0.x, t1.x-t0.x, t0.x-P.x),
                 vector3d(t2.y-t0.y, t1.y-t0.y, t0.y-P.y))
   if abs(u.z) < 1:
@@ -41,13 +50,14 @@ proc barycentric(t0, t1, t2: Vector2d, P: Vector2d): Vector3d =
 
   return vector3d(1.0 - (u.x+u.y) / u.z, u.y / u.z, u.x / u.z)
 
-proc triangle(image: var TGAImage, t0, t1, t2: Vector2d, color: TGAColor) =
+proc triangle(image: var TGAImage, zbuffer: var ZBuffer,
+              t0, t1, t2: Vector3d, color: TGAColor) =
   var minBoundingBox, clamp = vector2d(image.getWidth().float-1,
                                        image.getHeight().float-1)
 
   var maxBoundingBox = vector2d(0, 0)
 
-  template getBounds(vec: Vector2d) =
+  template getBounds(vec: Vector3d) =
     minBoundingBox.x = max(0, min(minBoundingBox.x, vec.x))
     maxBoundingBox.x = min(clamp.x, max(maxBoundingBox.x, vec.x))
 
@@ -63,7 +73,10 @@ proc triangle(image: var TGAImage, t0, t1, t2: Vector2d, color: TGAColor) =
       let bcScreen = barycentric(t0, t1, t2, vector2d(x.float, y.float))
       if bcScreen.x < 0 or bcScreen.y < 0 or bcScreen.z < 0:
         continue
-      doAssert image.set(x.cint, y.cint, color)
+      var z = t0.z*bcScreen.x + t1.z*bcScreen.y + t2.z*bcScreen.z
+      if zbuffer[x][y] < z:
+        zbuffer[x][y] = z
+        doAssert image.set(x.cint, y.cint, color)
 
 const
   width = 800
@@ -71,26 +84,31 @@ const
 var image = constructTGAImage(width, height, RGB.cint)
 
 var head = newModel("african_head.obj")
+
+var zbuffer = newZBuffer(width, height)
+
 # Shading.
 let lightDir = vector3d(0, 0, -1)
 for i in 0 .. <head.nfaces():
   let face = head.face(i)
-  var screenCoords: array[3, Vector2D]
+  var screenCoords: array[3, Vector3D]
   var worldCoords: array[3, Vector3D]
   for j in 0 .. <3:
     worldCoords[j] = head.vert(face[j])
-    screenCoords[j] = vector2d(
+    screenCoords[j] = vector3d(
       (worldCoords[j].x+1)*width / 2,
-      (worldCoords[j].y+1)*height / 2
+      (worldCoords[j].y+1)*height / 2,
+      worldCoords[j].z
     )
 
   var product = cross(worldCoords[2]-worldCoords[0],
-                      world_coords[1]-world_coords[0])
+                      worldCoords[1]-worldCoords[0])
   product.normalize()
   let intensity = dot(product, lightDir)
 
   if intensity > 0:
-    image.triangle(screenCoords[0], screenCoords[1], screenCoords[2],
+    image.triangle(zbuffer,
+                   screenCoords[0], screenCoords[1], screenCoords[2],
                    constructTGAColor(int(132*intensity), int(84*intensity),
                                      int(62*intensity), 255))
 
